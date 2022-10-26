d3441139074bb3d9541d17410bd0d6b55ee37875
skyi9 <88197965+skyi9@users.noreply.github.com>
2022-10-26 23:14:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+const App = () => {
+  const pageSize = 5;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<News key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route path="/business" element={<News key="business" pageSize={pageSize} country="in" category="business" />} />
+          <Route path="/general" element={<News key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route path="/entertainment" element={<News key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
+          <Route path="/sports" element={<News key="sports" pageSize={pageSize} country="in" category="sports" />} />
+          <Route path="/health" element={<News key="health" pageSize={pageSize} country="in" category="health" />} />
+          <Route path="/technology" element={<News key="technology" pageSize={pageSize} country="in" category="technology" />} />
+          <Route path="/science" element={<News key="science" pageSize={pageSize} country="in" category="science" />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
+
 }
 
 export default App;
